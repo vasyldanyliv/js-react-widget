6e409ec5a421ac4c139b8dc4fd2cd364be420fe9
vasyldanyliv <vasyldanyliv1201@gmail.com>
2018-03-07 11:29:07 +0200

    Made App.js by functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Components/Header/Header'
 
-class App extends Component {
-  render() {
-    return (
+const App = () =>
+    (
      <div className='container'>
        <div className='container-content'>
         <Header/>
         </div>
      </div>
-    )
-  }
-}
+    );
+
 export default App;
